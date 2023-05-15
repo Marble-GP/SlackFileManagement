b76dcf35ea5f823aa236aeb4389704822f6c0889
S.Watanabe <74084344+Marble-GP@users.noreply.github.com>
2023-05-15 13:45:53 +0900

    1ループ毎にクライアントを再接続するように変更

--- app_main.py
+++ app_main.py
@@ -62,7 +62,6 @@ if __name__ == "__main__":
             ssl._create_default_https_context = ssl._create_unverified_context
         else:
             exit(ssl_err.errno)
-
     
     if api_res["ok"]: #API test OK    
         
@@ -76,105 +75,129 @@ if __name__ == "__main__":
 
         timestamp_print("Slack API test is OK, File management app server started.")
 
+
+        del client
+
+
         while True:
-            #fetch new slack message information
-            pre_info = info.data.copy()
 
-            try:
-                info = client.conversations_list(types="public_channel,private_channel,im")
-            except SlackApiError as e:
-                timestamp_print(str(e))
-                timestamp_print("failed to get new conversations list.")
-
+            #open new client
+            client = WebClient(token=conf["API_TOKEN"])
             
-            if info["ok"]:
-                for d in info.data["channels"]:
-                    if d not in pre_info["channels"]:#message update detection
-                        try: 
-                            #get new file list
-                            #d["files"] = client.files_list(channel=d["id"]).data["files"]
-                            d["messages"] = client.conversations_history(channel=d["id"]).data["messages"]
+            try:
+                api_res = client.api_test()
+            except ssl.SSLCertVerificationError as ssl_err :
+                print(ssl_err)
+                if USE_UNVERIFIED_CONTEXT:
+                    timestamp_print("WARNING: Continue without SSL verification.")
+                    ssl._create_default_https_context = ssl._create_unverified_context
+                else:
+                    exit(ssl_err.errno)
+            
+            if api_res["ok"]:
+                #store old information
+                pre_info = info.data.copy()
 
-                        except SlackApiError as e:
-                            timestamp_print(str(e))
-                            if d["is_im"]:
-                                timestamp_print("failed to read im {}, id={}".format(d["user"], d["id"]))
-                            else:
-                                timestamp_print("failed to read channel {}, id={}".format(d["name"], d["id"]))
-                            continue
+                #fetch new slack message information    
+                try:
+                    info = client.conversations_list(types="public_channel,private_channel,im")
+                except SlackApiError as e:
+                    timestamp_print(str(e))
+                    timestamp_print("failed to get new conversations list.")
 
-                        for message_info in d["messages"]:
-                            if "files" not in message_info.keys():
-                                continue #pass the message
-                            
-                            #process each file sattached to a message
-                            for file_info in message_info["files"]:
-                                #check file types and extentions
-                                arrow_save = file_info["filetype"] not in conf["SMB_FILETYPE_REJECT_LIST"] \
-                                            and False not in [bool(re.fullmatch(os.path.splitext(file_info["name"])[1], ext, re.IGNORECASE) is None) for ext in conf["SMB_FILEEXT_REJECT_LIST"]]
+                
+                if info["ok"]:
+                    for d in info.data["channels"]:
+                        if d not in pre_info["channels"]:#message update detection
+                            try: 
+                                #get new file list
+                                #d["files"] = client.files_list(channel=d["id"]).data["files"]
+                                d["messages"] = client.conversations_history(channel=d["id"]).data["messages"]
 
-                                #download if has not been downloaded and appropriate file format
-                                if arrow_save:
-                                    dat = requests.get(file_info["url_private_download"], headers={'Authorization': 'Bearer '+conf["API_TOKEN"]}).content
-                                    
-                                    try: # refer user name from user ID
-                                        user_name = client.users_info(user=file_info["user"]).data["user"]["real_name"].replace(" ", "")    
-                                    except SlackApiError as e:
-                                        timestamp_print(str(e))
-                                        timestamp_print("failed to get an information from user ID:"+str(file_info["user"]))
-                                        continue
+                            except SlackApiError as e:
+                                timestamp_print(str(e))
+                                if d["is_im"]:
+                                    timestamp_print("failed to read im {}, id={}".format(d["user"], d["id"]))
+                                else:
+                                    timestamp_print("failed to read channel {}, id={}".format(d["name"], d["id"]))
+                                continue
 
-                                    #make target directory name and file name
-                                    date_ts = datetime.fromtimestamp(file_info["timestamp"])
-                                    target_dir_name1 = f"{date_ts.year}-{date_ts.month}"
-                                    target_dir_name2 = user_name
-                                    file_name = datetime.fromtimestamp(file_info["timestamp"]).strftime("%Y%m%d-%H%M%S") \
-                                                + f"_{user_name}_"+file_info["name"]
-                    
-                                    #make target path
-                                    target_path1 = os.path.join(conf["SMB_STORE_PATH"], target_dir_name1)
-                                    target_path2 = os.path.join(target_path1, target_dir_name2)
-                                    target_file_path = os.path.join(target_path2, file_name)
+                            for message_info in d["messages"]:
+                                if "files" not in message_info.keys():
+                                    continue #pass the message
+                                
+                                #process each file sattached to a message
+                                for file_info in message_info["files"]:
+                                    #check file types and extentions
+                                    arrow_save = file_info["filetype"] not in conf["SMB_FILETYPE_REJECT_LIST"] \
+                                                and False not in [bool(re.fullmatch(os.path.splitext(file_info["name"])[1], ext, re.IGNORECASE) is None) for ext in conf["SMB_FILEEXT_REJECT_LIST"]]
 
-                                    #open shared file server
-                                    with Smb(conf["SMB_USER"], conf["SMB_PASS"], conf["SMB_REMOTE_NAME"], conf["SMB_HOST"]) as smb_conn:
-
-                                        #Check if the file has already been downloaded
-                                        if not smb_conn.exists(conf["SMB_SERVICE_NAME"], target_file_path):
-                                            dat = requests.get(file_info["url_private_download"], headers={'Authorization': 'Bearer '+conf["API_TOKEN"]}).content
-                                        else: #already exist
-                                            # try:
-                                            #     if conf["SLACK_APP_ID"] in client.reactions_get(channel=d["id"], file=file_info["id"], timestamp=file_info["timestamp"]).data["message"]["reactions"]:
-                                            #         client.reactions_add(channel=d["id"], name="floppy_disk", timestamp=file_info["timestamp"])
-                                            # except SlackApiError as e:
-                                            #     timestamp_print(str(e))
-                                            #     timestamp_print("Stamp response failed.")
-
+                                    #download if has not been downloaded and appropriate file format
+                                    if arrow_save:
+                                        dat = requests.get(file_info["url_private_download"], headers={'Authorization': 'Bearer '+conf["API_TOKEN"]}).content
+                                        
+                                        try: # refer user name from user ID
+                                            user_name = client.users_info(user=file_info["user"]).data["user"]["real_name"].replace(" ", "")    
+                                        except SlackApiError as e:
+                                            timestamp_print(str(e))
+                                            timestamp_print("failed to get an information from user ID:"+str(file_info["user"]))
                                             continue
 
-                                        #make new month directory if not exist
-                                        if not smb_conn.exists(conf["SMB_SERVICE_NAME"], target_path1):
-                                            timestamp_print("make directry: "+target_path1)
-                                            smb_conn.makedirs(conf["SMB_SERVICE_NAME"], target_path1)
-                                        
-                                        #make new user name directory if not exist
-                                        if not smb_conn.exists(conf["SMB_SERVICE_NAME"], target_path2):
-                                            timestamp_print("make directry: "+target_path2)
-                                            smb_conn.makedirs(conf["SMB_SERVICE_NAME"], target_path2)
+                                        #make target directory name and file name
+                                        date_ts = datetime.fromtimestamp(file_info["timestamp"])
+                                        target_dir_name1 = f"{date_ts.year}-{date_ts.month}"
+                                        target_dir_name2 = user_name
+                                        file_name = datetime.fromtimestamp(file_info["timestamp"]).strftime("%Y%m%d-%H%M%S") \
+                                                    + f"_{user_name}_"+file_info["name"]
+                        
+                                        #make target path
+                                        target_path1 = os.path.join(conf["SMB_STORE_PATH"], target_dir_name1)
+                                        target_path2 = os.path.join(target_path1, target_dir_name2)
+                                        target_file_path = os.path.join(target_path2, file_name)
 
-                                        if not smb_conn.exists(conf["SMB_SERVICE_NAME"], target_file_path):
-                                            timestamp_print("save file: "+target_file_path)
-                                            if smb_conn.save_file(dat, conf["SMB_SERVICE_NAME"], target_file_path):
-                                                try:
-                                                    client.reactions_add(channel=d["id"], name="floppy_disk", timestamp=message_info["ts"])
-                                                    
-                                                except SlackApiError as e:
-                                                    timestamp_print(str(e))
-                                                    timestamp_print("Stamp response failed.")
-                                            else:
-                                                timestamp_print("Failed to save " +target_file_path) #error message
+                                        #open shared file server
+                                        with Smb(conf["SMB_USER"], conf["SMB_PASS"], conf["SMB_REMOTE_NAME"], conf["SMB_HOST"]) as smb_conn:
+
+                                            #Check if the file has already been downloaded
+                                            if not smb_conn.exists(conf["SMB_SERVICE_NAME"], target_file_path):
+                                                dat = requests.get(file_info["url_private_download"], headers={'Authorization': 'Bearer '+conf["API_TOKEN"]}).content
+                                            else: #already exist
+                                                # try:
+                                                #     if conf["SLACK_APP_ID"] in client.reactions_get(channel=d["id"], file=file_info["id"], timestamp=file_info["timestamp"]).data["message"]["reactions"]:
+                                                #         client.reactions_add(channel=d["id"], name="floppy_disk", timestamp=file_info["timestamp"])
+                                                # except SlackApiError as e:
+                                                #     timestamp_print(str(e))
+                                                #     timestamp_print("Stamp response failed.")
+
+                                                continue
+
+                                            #make new month directory if not exist
+                                            if not smb_conn.exists(conf["SMB_SERVICE_NAME"], target_path1):
+                                                timestamp_print("make directry: "+target_path1)
+                                                smb_conn.makedirs(conf["SMB_SERVICE_NAME"], target_path1)
+                                            
+                                            #make new user name directory if not exist
+                                            if not smb_conn.exists(conf["SMB_SERVICE_NAME"], target_path2):
+                                                timestamp_print("make directry: "+target_path2)
+                                                smb_conn.makedirs(conf["SMB_SERVICE_NAME"], target_path2)
+
+                                            if not smb_conn.exists(conf["SMB_SERVICE_NAME"], target_file_path):
+                                                timestamp_print("save file: "+target_file_path)
+                                                if smb_conn.save_file(dat, conf["SMB_SERVICE_NAME"], target_file_path):
+                                                    try:
+                                                        client.reactions_add(channel=d["id"], name="floppy_disk", timestamp=message_info["ts"])
+                                                        
+                                                    except SlackApiError as e:
+                                                        timestamp_print(str(e))
+                                                        timestamp_print("Stamp response failed.")
+                                                else:
+                                                    timestamp_print("Failed to save " +target_file_path) #error message
+                
+                else:# new client is not ok
+                    sleep(60.0)
 
             #wait a few second for the next processing step
+            del client
             sleep(2.0)
 
     else: #API test failure
